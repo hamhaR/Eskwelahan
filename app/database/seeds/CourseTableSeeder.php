0317011cc6cad3ae45768ee4cea240c1d9e650ad
hamhaR <waduud_a@yahoo.com>
2014-08-17 14:08:28 -0700

    modify CourseTableSeeder

--- app/database/seeds/CourseTableSeeder.php
+++ app/database/seeds/CourseTableSeeder.php
@@ -8,6 +8,23 @@ class CourseTableSeeder extends DatabaseSeeder{
                 'course_code' => 'CSC188',
                 'course_title' => 'Software Project Management',
                 'course_description' => 'a software project management course',
+                'course_section' => 'CS',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'course_code' => 'CSC186',
+                'course_title' => 'Human Computer Interaction',
+                'course_description' => 'a human computer interaction course for cs students',
+                'course_section' => 'CS',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'course_code' => 'HIST1',
+                'course_title' => 'Philippines History',
+                'course_section' => 'B5-1',
+                'course_description' => 'the history of the philippines',
                 'created_at' => new DateTime,
                 'updated_at' => new DateTime
             ]
