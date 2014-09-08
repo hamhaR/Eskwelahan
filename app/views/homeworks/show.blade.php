b88b3ef9ed4d9ee0963c8f963294de8c7eb4faf0
Christopher Peyton Rosen <beetlemon.cwts@gmail.com>
2014-09-08 16:47:32 +0800

    Partially reworked view of show homework

--- app/views/homeworks/show.blade.php
+++ app/views/homeworks/show.blade.php
@@ -1,2 +1,16 @@
 @extends("layout")
 @section("content")
+
+@if(Auth::check() && Auth::user()->role == 'teacher' && $homeworks[0]->teacher_id == Auth::user()->id)
+
+		<div class="col-md-6">
+			<h2>{{ $homeworks[0]->homework_title }}</h2>
+			<p>Posted on {{ date('j F Y, h:i A',strtotime($homeworks[0]->created_at)) }}</p>
+			@if($homeworks[0]->created_at != $homeworks[0]->updated_at)
+				<p>Last edit: {{ date('j F Y, h:i A',strtotime($homeworks[0]->updated_at)) }}</p>
+			@endif
+
+			{{ $homeworks[0]->homework_instruction }}
+		</div>
+
+@endif
