ff3d807808ade9d4fbdd91220005e60ae5380aba
Christopher Peyton Rosen <beetlemon.cwts@gmail.com>
2014-09-09 18:18:42 +0800

    Added view file for edit homework, reworked some methods

--- app/views/homeworks/show.blade.php
+++ app/views/homeworks/show.blade.php
@@ -5,18 +5,18 @@
 @if(Auth::check())
 
 		
-			<h2>{{ $homeworks[0]->homework_title }}</h2>
-			<p>Posted on {{ date('j F Y, h:i A',strtotime($homeworks[0]->created_at)) }}</p>
-			@if($homeworks[0]->created_at != $homeworks[0]->updated_at)
-				<p>Last edit: {{ date('j F Y, h:i A',strtotime($homeworks[0]->updated_at)) }}</p>
+			<h2>{{ $homeworks->homework_title }}</h2>
+			<p>Posted on {{ date('j F Y, h:i A',strtotime($homeworks->created_at)) }}</p>
+			@if($homeworks->created_at != $homeworks->updated_at)
+				<p>Last edit: {{ date('j F Y, h:i A',strtotime($homeworks->updated_at)) }}</p>
 			@endif
 
-			{{ $homeworks[0]->homework_instruction }}
+			{{ $homeworks->homework_instruction }}
 		
 
 
 
-	@if(Auth::user()->role == 'teacher' && $homeworks[0]->teacher_id == Auth::user()->id)
+	@if(Auth::user()->role == 'teacher' && $homeworks->teacher_id == Auth::user()->id)
 		<p>Buttons here.</p>
 
 	@endif
