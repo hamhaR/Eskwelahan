a492b9821c12f5e937a24f445da6cdd94951b019
vercillius <vercilliusjrmila@gmail.com>
2014-10-02 04:57:57 +0800

    dialog boxes for delete class added

--- app/views/class/index.blade.php
+++ app/views/class/index.blade.php
@@ -1,6 +1,7 @@
 @extends("layout")
 @section("content")
 <div class="container">
+@if(Auth::user()->role == 'teacher')
  <div class="row"> 
   <div class="col-md-3">
 <!-- Button trigger modal -->
@@ -9,6 +10,7 @@
 </button>
 </div>
 </div>
+@endif
 <br>
 <div class="row">
 @foreach($sections as $section)
@@ -28,17 +30,68 @@
                 <br>
                 
                 <br><br><br><br>
-                
-                {{ Form::open(array('url' => 'classes/' . $section->section_id, 'class' => 'pull-right')) }}
-                {{ Form::hidden('_method', 'DELETE') }}
-               <button type="submit" class="btn btn-danger">
+                @if(Auth::user()->role == 'teacher')
+               <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
                  <span class="glyphicon glyphicon-trash"></span>
                </button>
-                {{ Form::close() }}
+               <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
+                 <span class="glyphicon glyphicon-pencil"></span>
+               </button>
+                @endif
                 
               </div>
             </div>
     </div>
+
+
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Delete</h4>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to delete</p>
+               
+
+      </div>
+      <div class="modal-footer">
+
+         {{ Form::open(array('url' => 'classes/' . $section->section_id, 'class' => 'pull-right')) }}
+        {{ Form::hidden('_method', 'DELETE') }}
+         {{ Form::submit('Delete', array('class' => 'btn btn-primary')) }}
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        {{ Form::close() }}
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel">Edit</h4>
+      </div>
+      <div class="modal-body">
+        {{Form::open(array('url' => 'classes/'.$section->section_id, 'class' => 'pull-right'))}}
+        {{Form::text('section_name')}}        
+
+      </div>
+      <div class="modal-footer">
+         {{ Form::submit('Edit', array('class' => 'btn btn-primary')) }}
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        {{ Form::close() }}
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
 
   @endforeach
 @endforeach
@@ -87,36 +140,6 @@
 
 
 
-<div class="modal fade" id="addStudent" tabindex="-1" role="dialog" aria-labelledby="addStudentLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Add Student</h4>
-      </div>
-      <div class="modal-body">
-      
-      {{Form::open(array('url' => 'classes'))}}
-          <div class="form-group">
-           {{ Form::label('student_name', 'Name') }}
-            <select class="form-control" id="student_name" name="student_name">
-              @foreach(User::where('role','=','student')->get() as $student)
-                
-                <option>
-                  {{$student->fname}} {{$student->lname}}
-                </option>
-              
-              @endforeach
-            </select>
-          </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-        {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
-        {{Form::close()}}
-      </div>
-    </div>
-  </div>
-</div>
+
 
 </div>
