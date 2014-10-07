46ab5cd36d260ee33b9f51c9eba0f9840a0b7045
hamhaR <waduud_a@yahoo.com>
2014-10-06 23:18:33 -0700

    done -> manage course module

--- app/views/course/index.blade.php
+++ app/views/course/index.blade.php
@@ -17,47 +17,124 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?php $length = count($rows); ?>
-		@for ($i = 0; $i < $length; $i++)
-		<tr>
-			<td>{{ $rows[$i]['course_code'] }}</td>
-			<td>{{ $rows[$i]['course_title'] }}</td>
-			<td>{{ $rows[$i]['course_description'] }}</td>
-			<td>
-				<a class="btn btn-success" href="{{ URL::route('courses.edit', $rows[$i]['id']) }}"><span class="glyphicon glyphicon-pencil"></span> Edit Course</a>
-				<button class="btn btn-danger" data-toggle="modal" data-target="#deleteConfirmation"><span class="glyphicon glyphicon-remove"></span> Delete Course</button>
+		@foreach($rows as $key => $row)
+			<tr>
+				<td>{{ $row['course_code'] }}</td>
+				<td>{{ $row['course_title'] }}</td>
+				<td>{{ $row['course_description'] }}</td>
+				<td>
+					<button class="btn btn-success" data-toggle="modal" data-target="#edit{{$key}}"><span class="glyphicon glyphicon-pencil"></span> Edit Course</button>
+					<button class="btn btn-danger" data-toggle="modal" data-target="#deleteConfirmation{{$key}}"><span class="glyphicon glyphicon-remove"></span> Delete Course</button>
 				
-				<!-- Modal for Delete Course-->
-				<div class="modal fade" id="deleteConfirmation" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  					<div class="modal-dialog">
-    					<div class="modal-content">
-      						<div class="modal-header">
-        						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        						<h4 class="modal-title" id="myModalLabel">Warning</h4>
-      						</div>
-      						<div class="modal-body">
-        						<p>Are you sure you want to delete this course?</p>
-        						<p>This cannot be undone.</p>
-      						</div>
-      						<div class="modal-footer">
-        						{{ Form::open(array('url' => 'courses/' . $rows[$i]['id'])) }}
-          						{{ Form::hidden('_method', 'DELETE') }}
-          						{{ Form::submit('Yes', ['class' => 'btn btn-danger']) }}
+					<!-- Modal for Edit Course-->
+					<div class="modal fade" id="edit{{$key}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+									<h4 class="modal-title" id="myModalLabel">Edit Course</h4>
+								</div>
+								<div class="modal-body">
+									{{ Form::open(array('url' => 'courses/' . $row['id'], 'autocomplete' => 'off',
+										'class' => 'form-horizontal')) }}
+									<div class="form-group">
+										<label class="col-md-4 control-label" for="course_code"><span style="font-family:sans-serif; font-size:13px ">Course Code</span></label>
+										{{ $row['course_code'] }}
+									</div>
+									<div class="form-group">
+										<label class="col-md-4 control-label" for="course_code"><span style="font-family:sans-serif; font-size:13px ">Course Code</span></label>
+										{{ $row['course_title'] }}
+									</div>
+									<!-- Text input-->
+									<div class="form-group">
+										<label class="col-md-4 control-label" for="course_description"><span style="font-family:sans-serif; font-size:13px ">Course Description</span></label>  
+										<div class="col-md-8">
+											<input id="course_description" name="course_description" type="textarea" placeholder="" value="{{ $row['course_description'] }}" class="form-control input-md" required="">
+										</div>
+									</div>
+								</div>
+								<div class="modal-footer">
+									{{ Form::hidden('_method', 'PUT') }}
+									{{ Form::submit('Submit', ['class' => 'btn btn-default']) }}
 
-					          	<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-        						{{ Form::close() }}
-      						</div>
-    					</div>
-  					</div>
-				</div>
-				<!-- end of Modal for Delete Course -->
+									<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+									{{ Form::close() }}
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- end of Modal for Edit Course -->
 
-			</td>
-		</tr>
-		@endfor
+
+					<!-- Modal for Delete Course-->
+					<div class="modal fade" id="deleteConfirmation{{$key}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+									<h4 class="modal-title" id="myModalLabel">Warning</h4>
+								</div>
+								<div class="modal-body">
+									<p>Are you sure you want to delete this course?</p>
+									<p>This cannot be undone.</p>
+								</div>
+								<div class="modal-footer">
+									{{ Form::open(array('url' => 'courses/' . $row['id'])) }}
+									{{ Form::hidden('_method', 'DELETE') }}
+									{{ Form::submit('Yes', ['class' => 'btn btn-danger']) }}
+
+									<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+									{{ Form::close() }}
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- end of Modal for Delete Course -->
+
+				</td>
+			</tr>
+		@endforeach
 	</tbody>
 </table>
 
-<a class="btn btn-primary" href="{{ URL::route('courses.create') }}"><span class="glyphicon glyphicon-plus"></span> Create Course</a>
+	<button class="btn btn-primary" data-toggle="modal" data-target="#create"><span class="glyphicon glyphicon-plus"></span> Create Course</button>
 
 </div>
+
+<!-- Modal for Create Course-->
+<div class="modal fade" id="create" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h4 class="modal-title" id="myModalLabel">Create Course</h4>
+			</div>
+			<div class="modal-body">
+				{{ Form::open(array('url' => 'courses')) }}
+
+			    <div class="form-group">
+			        <label for="course_code">Course code</label>
+			        <input class="form-control" id="course_code" name="course_code" placeholder="Course code">
+			    </div>
+			    <div class="form-group">
+			        <label for="course_title">Course title</label>
+			        <input class="form-control" id="course_title" name="course_title" placeholder="Course title">
+			    </div>
+			    <div class="form-group">
+			        <label for="course_description">Course description</label>
+			        <input class="form-control" id="course_description" name="course_description" placeholder="Course description">
+			    </div>
+			    <div class="form-group">
+			        {{ Form::submit('Submit', ['class' => 'btn btn-default']) }}
+
+			        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+			    </div>
+
+				{{ Form::close() }}
+			</div>
+			<div class="modal-footer">
+			</div>
+		</div>
+	</div>
+</div>
+<!-- end of Modal for Create Course -->
