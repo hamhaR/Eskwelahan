934e6150b60ae0ac3807d71644941abd7cd6eeaf
Clyde Sacote <beetlemon.cwts@gmail.com>
2014-10-15 01:04:35 +0800

    Added link to Manage accounts on sidebar. Link will only appear if admin is logged in.

--- app/views/sidebar.blade.php
+++ app/views/sidebar.blade.php
@@ -1,6 +1,9 @@
 <div class=" sidebar">
   <ul class="nav nav-sidebar">
     <li><a href="/">Home</a></li>
+    @if(Auth::user()->role == 'admin')
+    	<li><a href="/manageaccounts">Manage Accounts</a></li>
+    @endif
     <li><a href="/editprofile">Edit Profile</a></li>
     <li><a href="/friends">Friends</a></li>
     <li><a href="/classes">Classes</a></li>
