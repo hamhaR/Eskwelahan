c389ac282048306e7326e7093b64b7119ea270b8
hamhaR <waduud_a@yahoo.com>
2014-10-15 13:30:28 -0700

    Admin manage courses

--- app/views/sidebar.blade.php
+++ app/views/sidebar.blade.php
@@ -3,12 +3,12 @@
     <li><a href="/">Home</a></li>
     @if(Auth::user()->role == 'admin')
     	<li><a href="/manageaccounts">Manage Accounts</a></li>
+        <li><a href="/courses">Courses</a></li>
     @endif
     <li><a href="/editprofile">Edit Profile</a></li>
     <li><a href="/friends">Friends</a></li>
     <li><a href="/classes">Classes</a></li>
     <li><a href="/tests">Tests</a></li>
-    <li><a href="/courses">Courses</a></li>
     <li><a href="/homeworks">Homeworks</a></li>
 	<li><a href="/materials">Materials</a></li>
     <li><a href="/logout">Logout</a></li>
