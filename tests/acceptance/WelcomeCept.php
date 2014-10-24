d71dfaff66a0ba38632dc419f21cee71315a0514
vercillius <vercilliusjrmila@gmail.com>
2014-10-24 18:08:52 +0800

    last

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -1,19 +1,19 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
-$I->amOnPage('/login');
-$I->fillField('username', 'juan');
-$I->fillField('password', 'tamad');
-$I->click('LOGIN');
-$I->see('Juan');
-$I->click('Logout');
+// <?php 
+// $I = new AcceptanceTester($scenario);
+// $I->wantTo('perform actions and see result');
+// $I->amOnPage('/login');
+// $I->fillField('username', 'juan');
+// $I->fillField('password', 'tamad');
+// $I->click('LOGIN');
+// $I->see('Juan');
+// $I->click('Logout');
 
 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
-$I->amOnPage('/');
-$I->fillField('username', 'juan');
-$I->fillField('password', 'masipag');
-$I->click('LOGIN');
-$I->see('Invalid username and/or password.');
+// $I = new AcceptanceTester($scenario);
+// $I->wantTo('perform actions and see result');
+// $I->amOnPage('/');
+// $I->fillField('username', 'juan');
+// $I->fillField('password', 'masipag');
+// $I->click('LOGIN');
+// $I->see('Invalid username and/or password.');
 
